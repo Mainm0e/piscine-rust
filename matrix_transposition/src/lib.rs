2009d62a4e5b2a982003fb490ac8f6942d755101
Mainm0e <mainm0e1996@gmail.com>
2023-08-21 21:46:09 +0300

    matrix_transposition // need to copi test case from main.rs and make mod test in lib.rs

---
+++ matrix_transposition/src/lib.rs
@@ -0,0 +1,24 @@
+// todo :matrix_transposition
+/* 
+Instructions
+Define a struct named Matrix as a tuple of 2 tuples. The nested tuple will contain 2 i32s.
+
+Create a function named transpose that calculates the transposition of a 2x2 matrix.
+
+pub fn transpose(m: Matrix) -> Matrix {
+}
+The transposition of a matrix, switches the columns to rows, and the rows to columns. For example:
+
+( a b )   __ transposition __>   ( a c )
+( c d )                          ( b d )
+Matrix must implement Debug, PartialEq and Eq. You can use derive.
+
+Remember that you are defining a library, so any element that can be called from an external crate must be made public. */
+
+// Path: src\lib.rs
+#[derive(Debug, PartialEq, Eq)]
+pub struct Matrix((i32, i32), (i32, i32));
+
+pub fn transpose(m: Matrix) -> Matrix {
+    Matrix((m.0 .0, m.1 .0), (m.0 .1, m.1 .1))
+}
