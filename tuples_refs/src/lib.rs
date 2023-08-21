5d3eb9a16bbdb8997bb8f198ec1d7c55cdd4f84a
Mainm0e <mainm0e1996@gmail.com>
2023-08-21 23:15:23 +0300

    tuples_refs

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// todo:tuples_refs
+/* tuples_refs
+Instructions
+Define a tuple struct to represent a Student. Each is identified by an id of type u32, their first name and last name.
+
+Then define three functions to return the id, first name and last name.
+
+pub fn id(student: &Student) -> u32 {
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn first_name(student: &Student) -> String {
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn last_name(student: &Student) -> String {
+} */
+
+#[derive(Debug)]
+pub struct Student(pub u32, pub String, pub String);
+
+pub fn id(student: &Student) -> u32 {
+    student.0
+}
+
+pub fn first_name(student: &Student) -> String {
+    student.1.clone()
+}
+
+pub fn last_name(student: &Student) -> String {
+    student.2.clone()
 }
