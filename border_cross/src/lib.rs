869ab39b1714512e4588864ab6b0a3408293409c
mainm0e <mainm0e1996@gmail.com>
2023-09-09 17:17:42 +0300

    quest 9 done 2 task is not gonna pass

--- border_cross/src/lib.rs
+++ border_cross/src/lib.rs
@@ -1,14 +1,109 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/* vehicles
+Instructions
+At a border crossing, you want to keep a list of all the vehicles that are waiting to enter the country. You intend to keep a waiting list of the vehicles, but the vehicles can be one of two types: Car or Truck.
+
+Create a function that receives a vector of structures that implements the Vehicle trait, and returns all the models.
+
+With the following structure:
+
+Expected Functions and Structures
+#[allow(dead_code)]
+pub struct Car<'a> {
+    pub plate_nbr: &'a str,
+    pub model: &'a str,
+    pub horse_power: u32,
+    pub year: u32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[allow(dead_code)]
+pub struct Truck<'a> {
+    pub plate_nbr: &'a str,
+    pub model: &'a str,
+    pub horse_power: u32,
+    pub year: u32,
+    pub load_tons: u32,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub trait Vehicle {
+    fn model(&self) -> &str;
+    fn year(&self) -> u32;
+}
+
+impl Vehicle for Truck<'_> {
+}
+
+impl Vehicle for Car<'_> {
+}
+
+fn all_models(list: Vec<&Vehicle>) -> Vec<&str> {
+}
+Usage
+Here is a program to test your function.
+
+fn main() {
+    let vehicles: Vec</*there is something missing here*/Vehicle> = vec![
+        &Car {
+            plate_nbr: "A3D5C7",
+            model: "Model 3",
+            horse_power: 325,
+            year: 2010,
+        },
+        &Truck {
+            plate_nbr: "V3D5CT",
+            model: "Ranger",
+            horse_power: 325,
+            year: 2010,
+            load_tons: 40,
+        },
+    ];
+    println!("{:?}", all_models(vehicles));
+}
+And its output
+
+$ cargo run
+["Model 3", "Ranger"]
+$ */
+
+#[allow(dead_code)]
+pub struct Car<'a> {
+    pub plate_nbr: &'a str,
+    pub model: &'a str,
+    pub horse_power: u32,
+    pub year: u32,
+}
+
+#[allow(dead_code)]
+pub struct Truck<'a> {
+    pub plate_nbr: &'a str,
+    pub model: &'a str,
+    pub horse_power: u32,
+    pub year: u32,
+    pub load_tons: u32,
+}
+
+pub trait Vehicle {
+    fn model(&self) -> &str;
+    fn year(&self) -> u32;
+}
+
+impl Vehicle for Truck<'_> {
+    fn model(&self) -> &str {
+        self.model
     }
+    fn year(&self) -> u32 {
+        self.year
+    }
+}
+
+impl Vehicle for Car<'_> {
+    fn model(&self) -> &str {
+        self.model
+    }
+    fn year(&self) -> u32 {
+        self.year
+    }
+}
+
+pub fn all_models(list: Vec<&dyn Vehicle>) -> Vec<&str> {
+    list.iter().map(|v| v.model()).collect()
 }
--- delete_prefix/src/lib.rs
+++ delete_prefix/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/* delete_prefix
+Instructions
+Define the function delete_prefix which returns the string slice s with the prefix removed. It should be wrapped in Some. If prefix is not a prefix of s, then delete_prefix returns None.
+
+Expected Function
+pub fn delete_prefix(prefix: &str, s: &str) -> Option<&str> {
 }
+Usage
+Here is a program to test your function.
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use delete_prefix::delete_prefix;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn main() {
+	println!("{:?}", delete_prefix("ab", "abcdefghijklmnop"));
+	println!("{:?}", delete_prefix("x", "abcdefghijklmnop"));
+}
+And its output:
+
+$ cargo run
+Some("cdefghijklmnop")
+None
+$ */
+
+pub fn delete_prefix<'a>(prefix: &'a str, s: &'a str) -> Option<&'a str> {
+    if s.starts_with(prefix) {
+        Some(&s[prefix.len()..])
+    } else {
+        None
     }
 }
+
